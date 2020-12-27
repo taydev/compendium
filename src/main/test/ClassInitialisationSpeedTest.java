c29486fea232a9a678d5fabe729f77bc4b14f630
Taylor Burn <tbu3bw@gmail.com>
2020-12-27 21:42:14 +0000

    holy shit we hit <1ms per load+modify+replace
    - Added new test code for testing the speed of update cycles for elements, current results: less than 1ms per update (dynamic loading ftw!)

--- src/main/test/ClassInitialisationSpeedTest.java
+++ src/main/test/ClassInitialisationSpeedTest.java
@@ -1,5 +1,6 @@
+import com.mongodb.client.model.Filters;
 import dev.compendium.core.ElementRegistry;
-
+import dev.compendium.core.util.Source;
 
 public class ClassInitialisationSpeedTest {
 
@@ -11,7 +12,19 @@ public class ClassInitialisationSpeedTest {
         ElementRegistry.getInstance();
         System.out.println("Initialised in: " + (System.currentTimeMillis() - subTime) + "ms");
         System.out.println("---------------------------------------------------------");
-
+        System.out.println("Performing 10000 load, edit, and re-save tests...");
+        subTime = System.currentTimeMillis();
+        for (int i = 0; i < 10000; i++) {
+            Source source = ElementRegistry.getInstance().findSourcesByOwnerID("277385484919898112").get(0);
+            if (i % 2 == 0) {
+                source.addDiscordID("627911250936070187");
+            } else {
+                source.removeDiscordID("627911250936070187");
+            }
+            ElementRegistry.getInstance().getSources().findOneAndReplace(Filters.eq("_id", source.getUUID()), source);
+        }
+        subTime = System.currentTimeMillis() - subTime;
+        System.out.println("10000 iterations complete. Time taken: " + subTime + "ms (" + (subTime / 1000) + " seconds)");
         System.out.println("---------------------------------------------------------");
         System.out.println("Total time taken: " + (System.currentTimeMillis() - time) + "ms");
     }
